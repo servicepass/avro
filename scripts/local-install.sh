53315edbf55dc3e98a7b5ea8d965b591ba53e1c6
Andy Le <anhld2@vng.com.vn>
2020-03-02 08:30:06 +0700

    Scripts: added scripts to locally install

---
+++ scripts/local-install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+mvn -Dproject.parent.basedir=`pwd` -Dproject.parent.parent.basedir=`pwd` -Dproject.parent.parent.parent.basedir=`pwd` -DskipTests=true -Drat.skip=true -Dcheckstyle.skip  install
