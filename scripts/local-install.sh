3e2ff23728a9a392fb59de7205c815a1b887632b
Andy Le <anhld2@vng.com.vn>
2020-03-02 09:21:03 +0700

    Scripts: refactored

--- scripts/local-install.sh
+++ scripts/local-install.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-mvn -Dproject.parent.basedir=`pwd` -Dproject.parent.parent.basedir=`pwd` -Dproject.parent.parent.parent.basedir=`pwd` -DskipTests=true -Drat.skip=true -Dcheckstyle.skip  install
+mvn \
+	-Dproject.parent.basedir=`pwd` \
+	-Dproject.parent.parent.basedir=`pwd` \
+	-Dproject.parent.parent.parent.basedir=`pwd` \
+	-DskipTests=true \
+	-Drat.skip=true \
+	-Dcheckstyle.skip \
+  install
